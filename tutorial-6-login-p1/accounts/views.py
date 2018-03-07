5d5afe2af1e3fcba18eeb412017c81b457d13d18
yogeshprasad <yogeshprasadkurmi@gmail.com>
2018-03-07 18:16:26 +0530

    Login Form Creation Part-1

---
+++ tutorial-6-login-p1/accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+	numbers = [1,2,3,4,5,6]
+	name = "ram"
+	args = {'name': name, 'numbers': numbers}
+	return render(request, 'accounts/home.html', args)
